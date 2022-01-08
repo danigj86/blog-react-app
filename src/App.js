758fbf367afcab5b5b434afb8107e3205538449c
Dani <guerrerojimenezdaniel@gmail.com>
2022-01-08 22:29:33 +0100

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react'
+import { useState } from "react";
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Home } from "./components/Home"
 import { Login } from "./components/Login"
 import { CreatePost } from "./components/CreatePost"
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
+
+
+
 
 function App() {
-  return (
-    <Router>
-      <nav>
-        <Link to='/Home'>Home</Link>
-        <Link to='/createpost'>Create Post</Link>
-        <Link to='/login'>Login</Link>
-      </nav>
-      <Routes>
-        <Route path='/login' element={ <Login /> }/>
-        <Route path='/createpost' element={ <CreatePost/> }/>
-        <Route path='/' element={ <Home/> }/>
-        <Route path='/*' element={ <Home/> }/>
-      </Routes>
-    </Router>
-  );
-}
 
-export default App;
+  const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'));
+
+  const logOut = () => {
+
+    signOut(auth)
+      .then(() => {
+
+        //localStorage.clear();
+        localStorage.removeItem('currentUser');
+        localStorage.removeItem('isAuth');
+        setIsAuth(false);
+        window.location.pathname = "/login";
+      })
+    }
+    return (
+      <Router>
+        <nav>
+          <Link to='/Home'>Home</Link>
+          {
+            isAuth && <Link to='/createpost'>Create Post</Link>
+          }
+
+          {
+            !isAuth ? <Link to='/login'>Login</Link> : <button onClick={logOut}>Sign Out</button>
+          }
+
+          <span>
+            {
+              isAuth && localStorage.getItem('currentUser')
+            }
+          </span>
+        </nav>
+        <Routes>
+          <Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
+          <Route path='/createpost' element={<CreatePost />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/*' element={<Home />} />
+        </Routes>
+      </Router>
+    );
+  }
+
+  export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-export const Login = () => {
+import {auth, provider} from '../firebase-config'
+import {signInWithPopup} from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+
+export const Login = ({setIsAuth}) => {
+
+    let navigate =  useNavigate();
+
+    const signInWithGoogle = () =>{
+         signInWithPopup(auth, provider)
+         .then((result)=>{
+           //console.log(result.user.auth.currentUser.displayName);
+           localStorage.setItem('isAuth', true);
+           localStorage.setItem('currentUser', result.user.auth.currentUser.displayName);
+      
+           setIsAuth(true);
+           navigate('/',{
+            replace:true
+        });
+         }); 
+         
+    }
+
+
     return (
-        <div>
-            Login
+        <div className="loginPage">
+            <p>Sign In With Google to Continue</p>
+            <button className="login-with-google-btn" onClick={signInWithGoogle}>
+                Sign in with Google
+            </button>
         </div>
     )
 }
