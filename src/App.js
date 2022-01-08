f64bf30aecb524f96507e2bf7292666636e75cda
Dani <guerrerojimenezdaniel@gmail.com>
2022-01-08 20:54:26 +0100

    Navbar Añadido

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { Home } from "./components/Home"
+import { Login } from "./components/Login"
+import { CreatePost } from "./components/CreatePost"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to='/Home'>Home</Link>
+        <Link to='/createpost'>Create Post</Link>
+        <Link to='/login'>Login</Link>
+      </nav>
+      <Routes>
+        <Route path='/login' element={ <Login /> }/>
+        <Route path='/createpost' element={ <CreatePost/> }/>
+        <Route path='/' element={ <Home/> }/>
+        <Route path='/*' element={ <Home/> }/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/firebase-config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+import { getAnalytics } from "firebase/analytics";
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMC6-owzK-xpdDAP406IrsN1rX36dUXDw",
+  authDomain: "blog-react-app-4b2c8.firebaseapp.com",
+  projectId: "blog-react-app-4b2c8",
+  storageBucket: "blog-react-app-4b2c8.appspot.com",
+  messagingSenderId: "400170305190",
+  appId: "1:400170305190:web:01bf987e8e24339c9f1667",
+  measurementId: "G-KCTC1CC4EE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const provider  = GoogleAuthProvider();
+export const db = getFirestore(app);
